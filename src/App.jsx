d8986167ce8ee61a608f8375d2a435f8f2b7d00b
gabrielfcoelh83 <gabrielfcoelh83@gmail.com>
2025-08-25 17:19:25 -0300

    Introdução aos componentes

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+  import TextInput from './components/TexInput.jsx'
+  
+  function App() { 
+    return (
+      <div>
+        <TextInput />
+        <TextInput />
+        <TextInput />
+      </div>
+    );
+  }
+ 
+export default App
