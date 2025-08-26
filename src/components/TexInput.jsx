d32427bddef8fb6877e20a9888db01276dec692f
gabrielfcoelh83 <gabrielfcoelh83@gmail.com>
2025-08-26 10:39:38 -0300

    Exemplos de formulários com componentes controlados e não controlados

--- src/components/TexInput.jsx
+++ src/components/TexInput.jsx
@@ -1,28 +1,39 @@
 import { useState } from 'react';
 import styles from './TextInput.module.css';
 
-export default function TexInput({placeholder, maxLength, ...props}) {
-const [text, setText] = useState("");
+export default function TexInput({ placeholder, maxLength, ...props }) {
+  const [text, setText] = useState("");
+  const [tweetList, setTweetList] = useState([]);
 
-function onTextChange(event) {
-  const text = event.target.value;
-  if (text.length <= maxLength) {
-    setText(text);
+  function onTextChange(event) {
+    const newText = event.target.value;
+    if (newText.length <= maxLength) {
+      setText(newText);
+    }
   }
-}
+
+  function sendTweet(event) {
+    setTweetList([...tweetList, text]);
+  }
 
   return (
-    <div> 
-      <textarea 
-        className={styles.input} 
-        placeholder={placeholder} 
-        maxLength={maxLength} 
-        value={text} 
-        onChange={onTextChange} 
-        {...props} 
+    <div onSubmit={sendTweet}>
+      <textarea
+        className={styles.input}
+        placeholder={placeholder}
+        maxLength={maxLength}
+        value={text}
+        onChange={onTextChange}
+        {...props}
       />
-
       <p>{text.length} / {maxLength}</p>
+      <button on onClick={sendTweet}>Enviar</button>
+      {tweetList.map(tweet => {
+        return (
+          <p>{tweet}</p>
+        )
+      })}
+      
     </div>
   );
 }
