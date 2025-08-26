4bb4eb761ecd6b735f8e7574dd8d7843fddf41be
gabrielfcoelh83 <gabrielfcoelh83@gmail.com>
2025-08-26 09:42:26 -0300

    Estados, Funções e eventos

--- src/components/TexInput.jsx
+++ src/components/TexInput.jsx
@@ -1,11 +1,28 @@
+import { useState } from 'react';
 import styles from './TextInput.module.css';
 
 export default function TexInput({placeholder, maxLength, ...props}) {
+const [text, setText] = useState("");
 
+function onTextChange(event) {
+  const text = event.target.value;
+  if (text.length <= maxLength) {
+    setText(text);
+  }
+}
 
   return (
     <div> 
-      <textarea className={styles.input} placeholder={placeholder} maxLength={maxLength} {...props} />
+      <textarea 
+        className={styles.input} 
+        placeholder={placeholder} 
+        maxLength={maxLength} 
+        value={text} 
+        onChange={onTextChange} 
+        {...props} 
+      />
+
+      <p>{text.length} / {maxLength}</p>
     </div>
   );
 }
