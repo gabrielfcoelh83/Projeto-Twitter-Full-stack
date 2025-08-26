3f1e7b05d80bced2716eaa8a0f1b36ad60fd59d8
gabrielfcoelh83 <gabrielfcoelh83@gmail.com>
2025-08-26 11:21:23 -0300

    Configurando o componente TextInput

--- src/components/TexInput.jsx
+++ src/components/TexInput.jsx
@@ -1,16 +1,8 @@
 import { useState } from 'react';
 import styles from './TextInput.module.css';
 
-export default function TexInput({ placeholder, maxLength, ...props }) {
-  const [text, setText] = useState("");
+export default function TexInput(props) {
   const [tweetList, setTweetList] = useState([]);
-
-  function onTextChange(event) {
-    const newText = event.target.value;
-    if (newText.length <= maxLength) {
-      setText(newText);
-    }
-  }
 
   function sendTweet(event) {
     setTweetList([...tweetList, text]);
@@ -20,19 +12,17 @@ export default function TexInput({ placeholder, maxLength, ...props }) {
     <div onSubmit={sendTweet}>
       <textarea
         className={styles.input}
-        placeholder={placeholder}
-        maxLength={maxLength}
-        value={text}
-        onChange={onTextChange}
         {...props}
       />
-      <p>{text.length} / {maxLength}</p>
-      <button on onClick={sendTweet}>Enviar</button>
+     
+      {
+      /* <button on onClick={sendTweet}>Enviar</button>
       {tweetList.map(tweet => {
         return (
           <p>{tweet}</p>
         )
-      })}
+      })} */
+      }
       
     </div>
   );
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,16 +1,31 @@
 import gabrielImg from '../assets/gabriel.png';
 import TexInput from '../components/TexInput.jsx';
+import { useState } from 'react';
 
 export default function Index() {
+  const [text, setText] = useState("");
+  const maxLength = 125;
+
+   function onTextChange(event) {
+    const newText = event.target.value;
+    if (newText.length <= maxLength) {
+      setText(newText);
+    }
+  }
+
   return (
     <div>
       <h1>Treina Twitter</h1>
       <div> 
         <img src={gabrielImg} alt="Gabriel" />
-        <TexInput placeholder="O que está acontecendo?" maxLength={125} />
+        <TexInput placeholder={"O que está acontecendo?"} 
+        maxLength={125} 
+        onChange={onTextChange} 
+        value={text} 
+        />
       </div>
       <div>
-        <div>0 / 125</div>
+        <div>{text.length} / {maxLength}</div>
         <button>Tweetar</button>
       </div>
       <ul>
